319105ab6f818560fc3fac881f9648dd14b43020
María Evangelina Ferreira <evaferreira92@hotmail.com>
2019-05-22 10:45:03 -0300

    :nail_care:

--- chroma.js
+++ chroma.js
@@ -3,90 +3,90 @@ c1 = document.getElementById("canvas1");
 ctx1 = this.c1.getContext("2d");
 
 // Get video
-navigator.mediaDevices.getUserMedia({video: true})
-  .then(function (stream) {
-    // Put video input into video tag
-    video.srcObject = stream;
+navigator.mediaDevices.getUserMedia({ video: true })
+    .then(function (stream) {
+        // Put video input into video tag
+        video.srcObject = stream;
 
-    let self = this;
-    
-    this.video.addEventListener("loadeddata", function() {
-      self.width = self.video.videoWidth;
-      self.height = self.video.videoHeight;
+        let self = this;
 
-      self.time_callback();
+        this.video.addEventListener("loadeddata", function () {
+            self.width = self.video.videoWidth;
+            self.height = self.video.videoHeight;
 
-    }, false);
-  })
-  .catch(function (error) {
-      console.log("Error:", error);
-  });
+            self.time_callback();
 
-function draw () {
-  ctx1.drawImage(video, 0, 0, this.width, this.height);
+        }, false);
+    })
+    .catch(function (error) {
+        console.log("Error:", error);
+    });
+
+function draw() {
+    ctx1.drawImage(video, 0, 0, this.width, this.height);
 }
 
-function time_callback () {
-  if (video.paused || video.ended) {
-    return;
-  }
+function time_callback() {
+    if (video.paused || video.ended) {
+        return;
+    }
 
-  draw();
-  findColor(color);
+    draw();
+    findColor(color);
 
-  // Update
-  setTimeout(function () {
-    this.time_callback();
-  }, 0);
+    // Update all the time, otherwise the canvas won't be up to date with the video
+    setTimeout(function () {
+        this.time_callback();
+    }, 0);
 };
 
+function pickedFilter() {
+    var selector = document.getElementById("colorFilter");
+    color = selector.options[selector.selectedIndex].value;
 
-function pickedFilter () {
-	var selector = document.getElementById("colorFilter");
-  color = selector.options[selector.selectedIndex].value;
-
-  time_callback();
+    time_callback();
 }
 
 // Which color? Green or blue?
 pickedFilter();
 colorFilter.addEventListener("change", pickedFilter);
 
-function findColor (color) {
-  let frame = this.ctx1.getImageData(0, 0, this.width, this.height);
+function findColor(color) {
+    let frame = this.ctx1.getImageData(0, 0, this.width, this.height);
 
-  let l = frame.data.length / 4;
-  
-	if (color === "Green") {
-    for (let i = 0; i < l; i++) {
-    
-      let r = frame.data[i * 4 + 0];
-      let g = frame.data[i * 4 + 1];
-      let b = frame.data[i * 4 + 2];
+    let l = frame.data.length / 4;
 
-      if (g > r && g > b) {
-        // If it's too dark it's probably not right
-        if (g > 100 ) {
-          // Alpha value to 0
-          frame.data[i * 4 + 3] = 0;
+    if (color === "Green") {
+        for (let i = 0; i < l; i++) {
+
+            let r = frame.data[i * 4 + 0];
+            let g = frame.data[i * 4 + 1];
+            let b = frame.data[i * 4 + 2];
+
+            if (g > r && g > b) {
+                // If it's too dark it's probably not right
+                if (g > 100) {
+                    // Alpha value to 0
+                    frame.data[i * 4 + 3] = 0;
+                }
+            }
         }
-      }
-    }
-  } else {
-    for (let i = 0; i < l; i++) {
-    
-      let r = frame.data[i * 4 + 0];
-      let g = frame.data[i * 4 + 1];
-      let b = frame.data[i * 4 + 2];
+    } else {
+        for (let i = 0; i < l; i++) {
 
-      if (b > r && b > g) {
-        if (b > 100 ) {
-          frame.data[i * 4 + 3] = 0;
+            let r = frame.data[i * 4 + 0];
+            let g = frame.data[i * 4 + 1];
+            let b = frame.data[i * 4 + 2];
+
+            if (b > r && b > g) {
+                if (b > 100) {
+                    frame.data[i * 4 + 3] = 0;
+                }
+            }
         }
-      }
     }
-  }
 
-  this.ctx1.putImageData(frame, 0, 0);
-  return;
+    // Put the modified data back into the canvas
+    this.ctx1.putImageData(frame, 0, 0);
+    return;
 }
